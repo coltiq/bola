9d42a910f08549d80ad88e10cc978549fd2118d6
coltiq <dev@coltiq.com>
2024-07-10 18:06:43 -0500

    update

--- install/terminal/04-docker.sh
+++ install/terminal/04-docker.sh
@@ -1,8 +1,3 @@
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
-sudo rm -rf /var/lib/docker
-sudo rm -rf /var/lib/containerd
-
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install ca-certificates curl
