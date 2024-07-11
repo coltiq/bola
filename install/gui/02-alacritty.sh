e960d96ac64b10960d0afcfc6196184c48c7b9bc
coltiq <dev@coltiq.com>
2024-07-10 20:14:48 -0500

    update

--- install/gui/02-alacritty.sh
+++ install/gui/02-alacritty.sh
@@ -5,3 +5,4 @@ mkdir -p ~/.config/alacritty
 cp ~/.local/share/bola/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/.local/share/bola/themes/catppuccin-mocha/alacritty.toml ~/.config/alacritty/theme.toml
 cp ~/.local/share/omakub/configs/alacritty/fonts/MesloLGLDZ.toml ~/.config/alacritty/font.toml
+cp ~/.local/share/omakub/configs/alacritty/font-size.toml ~/.config/alacritty/font-size.toml
