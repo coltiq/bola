5034d5f67218c4a7710db740148f198f9854f9fb
coltiq <dev@coltiq.com>
2024-07-10 20:26:35 -0500

    update

--- install/gui/02-alacritty.sh
+++ install/gui/02-alacritty.sh
@@ -1,6 +1,7 @@
 # Alacritty terminal: https://alacritty.org/
 sudo apt install -y alacritty
 mkdir -p ~/.config/alacritty
+echo "Hello"
 
 cp ~/.local/share/bola/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/.local/share/bola/themes/catppuccin-mocha/alacritty.toml ~/.config/alacritty/theme.toml
