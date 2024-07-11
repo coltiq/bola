204bcce5c09f4e5a6d61ab31e176b9bf79bd735c
coltiq <dev@coltiq.com>
2024-07-11 14:07:39 -0500

    update

--- boot.sh
+++ boot.sh
@@ -1,8 +1,27 @@
 set -e
 
+ascii_logo='
+______  ______         ____    _____                _____         
+\     \|\     \    ____\_  \__|\    \             /      |_       
+ |     |\|     |  /     /     \\\    \           /         \      
+ |     |/____ /  /     /\      |\\    \         |     /\    \     
+ |     |\     \ |     |  |     | \|    | ______ |    |  |    \    
+ |     | |     ||     |  |     |  |    |/      \|     \/      \   
+ |     | |     ||     | /     /|  /            ||\      /\     \  
+/_____/|/_____/||\     \_____/ | /_____/\_____/|| \_____\ \_____\ 
+|    |||     | || \_____\   | / |      | |    ||| |     | |     | 
+|____|/|_____|/  \ |    |___|/  |______|/|____|/ \|_____|\|_____| 
+                  \|____|                                         
+'
+
+echo -e "$ascii_logo"
+echo "=> ONLY for FRESH installations of Ubuntu 24.04"
+echo -e "\nUpdating (ctrl+c to abort)...\n"
+
 sudo apt-get update >/dev/null
 sudo apt-get install git -y >/dev/null
 
+echo "Downloading Bola..."
 rm -rf ~/.local/share/bola
 git clone https://github.com/coltiq/bola.git ~/.local/share/bola >/dev/null
 
