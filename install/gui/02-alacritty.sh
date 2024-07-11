d9246a9986c720e1fac506b6dcb5941e9d7b4f2f
coltiq <dev@coltiq.com>
2024-07-10 21:12:18 -0500

    update

--- install/gui/02-alacritty.sh
+++ install/gui/02-alacritty.sh
@@ -1,10 +1,8 @@
-set -e
 # Alacritty terminal: https://alacritty.org/
 sudo apt install -y alacritty
 mkdir -p ~/.config/alacritty
-echo "Hello"
 
-cp ~/.local/share/bola/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
-cp ~/.local/share/bola/themes/catppuccin-mocha/alacritty.toml ~/.config/alacritty/theme.toml
-cp ~/.local/share/bola/configs/alacritty/fonts/MesloLGLDZ.toml ~/.config/alacritty/font.toml
-cp ~/.local/share/bola/configs/alacritty/font-size.toml ~/.config/alacritty/font-size.toml
+#cp ~/.local/share/bola/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
+#cp ~/.local/share/bola/themes/catppuccin-mocha/alacritty.toml ~/.config/alacritty/theme.toml
+#cp ~/.local/share/bola/configs/alacritty/fonts/MesloLGLDZ.toml ~/.config/alacritty/font.toml
+#cp ~/.local/share/bola/configs/alacritty/font-size.toml ~/.config/alacritty/font-size.toml
